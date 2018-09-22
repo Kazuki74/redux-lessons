e92bea8dc88e494dd56e846b22ed447739a0e846
小林和希 <kobayashi.kazuki74@gmail.com>
2018-09-22 14:04:58 +0900

    implemented fetchGiphyApi action

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
 
 class App extends Component {
+  fetchGiphyApi = () => {
+    const query = "cat";
+    const key = "LARVnBHiwkJAJhZevDhFimwLgw00m7rJ";
+    const limit = "5"
+
+    const url = `http://api.giphy.com/v1/gifs/search?q=${query}&api_key=${key}&limit=${limit}`
+
+    axios.get(url).then(res => {
+      console.log(res.data.data[0].images.downsized.url)
+    })
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title" onClick={this.fetchGiphyApi} >Welcome to React</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
