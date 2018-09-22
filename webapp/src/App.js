b61a68b1da25fb40a46cb5f140c8a0da4adc5559
小林和希 <kobayashi.kazuki74@gmail.com>
2018-09-22 14:45:31 +0900

    implemented search function

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,30 +1,45 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Search from './components/Search'
 import axios from "axios";
 
 class App extends Component {
-  fetchGiphyApi = () => {
-    const query = "cat";
+  constructor(props) {
+    super(props);
+    this.state = {
+      gifUrllist : [],
+    };
+  }
+  fetchGiphyApi = (terget) => {
+    const query = terget;
     const key = "LARVnBHiwkJAJhZevDhFimwLgw00m7rJ";
     const limit = "5"
 
     const url = `http://api.giphy.com/v1/gifs/search?q=${query}&api_key=${key}&limit=${limit}`
 
     axios.get(url).then(res => {
-      console.log(res.data.data[0].images.downsized.url)
+      const data = res.data.data;
+      const imageUrlList = data.map(item => item.images.downsized.url);
+      this.setState({gifUrllist: imageUrlList})
     })
+    console.log(this.state.gifUrllist)
+  }
+  renderImageList = (list) => {
+    const imageList = list.map((url,index) => {
+      return(
+        <li key={index}>
+          <img src={url} alt="gif" />
+        </li>
+      )
+    })
+    return <ul>{imageList}</ul>
   }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title" onClick={this.fetchGiphyApi} >Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Search search={this.fetchGiphyApi}/>
+        <h1 className="App-title" onClick={this.fetchGiphyApi} >Welcome to React</h1>
+        <div>{this.renderImageList(this.state.gifUrllist)}</div>
       </div>
     );
   }
