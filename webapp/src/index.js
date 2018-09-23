85df8f16266da3bb52bf594ff39d1f1a153cf8e0
小林和希 <kobayashi.kazuki74@gmail.com>
2018-09-23 13:25:45 +0900

    implemented plus and minus action

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -12,11 +12,11 @@ store.subscribe(() => {
 	console.log(store.getState());
 });
 
-store.dispatch({type: "PLUS_ONE"});
+store.dispatch({type: "PLUS", payload: {num: 3}});
 
-store.dispatch({type: "MINUS_ONE"});
+store.dispatch({type: "PLUS", payload: {num: 3}});
 
-store.dispatch({type: "MINUS_ONE"});
+store.dispatch({type: "MINUS", payload: {num: 3}});
 
 ReactDOM.render(
 	<App />,
--- webapp/src/reducer.js
+++ webapp/src/reducer.js
@@ -1,9 +1,10 @@
 const reducer = (state = 0, action) => {
 	switch (action.type) {
-		case "PLUS_ONE":
-			return state + 1;
-		case "MINUS_ONE":
-			return state - 1;
+		case "PLUS":
+			console.log(action)
+			return state + action.payload.num;
+		case "MINUS":
+			return state - action.payload.num;
 		default:
 			return state;
 	}
