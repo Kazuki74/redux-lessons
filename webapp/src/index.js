b3386e6e0a015eabaae152ece1575b67fdb64408
小林和希 <kobayashi.kazuki74@gmail.com>
2018-09-23 13:19:41 +0900

    created store and reducer

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -3,6 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import reducer from './reducer';
+import { createStore } from "redux";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducer);
+
+store.subscribe(() => {
+	console.log(store.getState());
+});
+
+store.dispatch({type: "PLUS_ONE"});
+
+store.dispatch({type: "MINUS_ONE"});
+
+store.dispatch({type: "MINUS_ONE"});
+
+ReactDOM.render(
+	<App />,
+	document.getElementById('root')
+);
 registerServiceWorker();
