3610e425fe6f854f8bd30c5a7ae6598a23320816
小林和希 <kobayashi.kazuki74@gmail.com>
2018-09-23 13:47:40 +0900

    connect store and dispatch to component

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -5,21 +5,14 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import reducer from './reducer';
 import { createStore } from "redux";
+import { Provider } from "react-redux";
 
 const store = createStore(reducer);
 
-store.subscribe(() => {
-	console.log(store.getState());
-});
-
-store.dispatch({type: "PLUS", payload: {num: 3}});
-
-store.dispatch({type: "PLUS", payload: {num: 3}});
-
-store.dispatch({type: "MINUS", payload: {num: 3}});
-
 ReactDOM.render(
-	<App />,
+	<Provider store = {store}>
+		<App />
+	</Provider>,
 	document.getElementById('root')
 );
 registerServiceWorker();
