53371886c0e7dade46a90e79a3ddb2c46c3a0884
小林和希 <kobayashi.kazuki74@gmail.com>
2018-09-23 13:03:47 +0900

    first commit

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,21 +1,21 @@
 import React, { Component } from 'react';
-import { connect } from "react-redux";
+import logo from './logo.svg';
+import './App.css';
 
-const App = ({ someState, doSomething }) => {
-  return <div onClick={() => doSomething("some")}>{someState}</div>
-}
-
-const mapStateToProps = state => {
-  return {
-    someState: state
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
   }
 }
-const mapDispatchToProps = dispatch => {
-  return {
-    doSomething: some => {
-      dispatch({ type: "ADD", payload: some })
-    }
-  }
-}
 
-export default connect(mapStateToProps, mapDispatchToProps)(App)
+export default App;
