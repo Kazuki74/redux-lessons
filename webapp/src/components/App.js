d304d961f7c04f699e4e189e77d05239667f6ee7
小林和希 <kobayashi.kazuki74@gmail.com>
2018-09-25 13:03:37 +0900

    divided App.js into components and containers

---
+++ webapp/src/components/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+const App = ({number, plus, minus}) => (
+  <div>
+    <h2>App {number}</h2>
+    <button onClick={()=>{plus(10)}}>+10</button>
+    <button onClick={()=>{minus(10)}}>-10</button>
+  </div>
+)
+
+export default App;
